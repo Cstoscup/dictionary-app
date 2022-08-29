d21005ba965cd006b628bc1bc89c0dd7b720e60e
Cstoscup <109521820+Cstoscup@users.noreply.github.com>
2022-08-29 14:50:16 -0400

    Added meanings/definitions/parts of speech/and examples

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import React, {useState} from "react";
+import Results from "./Results"
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState("");
+    const [results, setResults] = useState(null);
 
     function handleChange(event) {
         event.preventDefault();
@@ -11,19 +13,21 @@ export default function Dictionary() {
 
     function handleClick(event) {
         event.preventDefault();
-        alert(`Searching for "${keyword}"...`);
         let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
         axios.get(url).then(handleResponse);
     }
 
     function handleResponse(response) {
-        console.log(response)
+        setResults(response.data[0]);
     }
     
     return (
-        <form className="text-center mt-5 mb-5">
-            <input type="search" placeholder="Enter word..." className="me-3" onChange={handleChange} />
-            <input type="submit" value="Search" onClick={handleClick} />
-        </form>
+        <div className="Dictionary">
+            <form className="text-center mt-5 mb-5">
+                <input type="search" placeholder="Enter word..." className="me-3" onChange={handleChange} />
+                <input type="submit" value="Search" onClick={handleClick} />
+            </form>
+            <Results data={results} />
+        </div>
     )
 }
